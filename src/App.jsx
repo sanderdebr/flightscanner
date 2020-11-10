b9c82d0536fbdb50546eaa135f9a27197fd590f7
sanderdebr <sander606@gmail.com>
2020-11-10 14:27:30 +0100

    started on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { orange } from '@material-ui/core/colors';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import Header from './Header';
+import SearchContainer from './SearchContainer';
 
-const App = ({ title = 'FlightScanner' }) => <div>{title}</div>;
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Continue reading…',
+};
 
-export default App;
+const theme = createMuiTheme({
+  status: {
+    danger: orange[500],
+  },
+});
+
+export default function Blog() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header title="FlightScanner" />
+        <SearchContainer post={mainFeaturedPost}>
+          <Typography
+            component="h1"
+            variant="h3"
+            color="inherit"
+            gutterBottom
+          >
+            Let the journey begin
+          </Typography>
+          {/* <Search /> */}
+        </SearchContainer>
+      </ThemeProvider>
+    </>
+  );
+}
---
+++ src/SearchContainer.jsx
@@ -0,0 +1,73 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+}));
+
+export default function MainFeaturedPost(props) {
+  const classes = useStyles();
+  const { post } = props;
+
+  return (
+    <Paper
+      className={classes.mainFeaturedPost}
+      style={{ backgroundImage: `url(${post.image})` }}
+    >
+      <Container maxWidth="lg">
+        <div className={classes.overlay} />
+        <Grid container>
+          <Grid item md={6}>
+            <div className={classes.mainFeaturedPostContent}>
+              <Typography
+                component="h1"
+                variant="h3"
+                color="inherit"
+                gutterBottom
+              >
+                {post.title}
+              </Typography>
+              <Typography variant="h5" color="inherit" paragraph>
+                {post.description}
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </Paper>
+  );
+}
+
+MainFeaturedPost.propTypes = {
+  post: PropTypes.objectOf().isRequired,
+};
