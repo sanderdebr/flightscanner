36c6d6aaf62e352d2fb4e00f7babb788c4b9c1f8
sanderdebr <sander606@gmail.com>
2020-12-04 16:10:55 +0100

    deployment fixes

--- frontend/build-utils/webpack.prod.js
+++ frontend/build-utils/webpack.prod.js
@@ -1,14 +1,13 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+// const path = require('path');
+// const Dotenv = require('dotenv-webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   plugins: [
-    new Dotenv({
-      path: path.resolve(__dirname, '..', './.env.production'),
-    }),
-    new CompressionPlugin()
+    // new Dotenv({
+    //   path: path.resolve(__dirname, '..', './.env.production'),
+    // }),
+    new CompressionPlugin(),
   ],
 };
- 
